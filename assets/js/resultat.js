fa6f8f0ddb3343eebd2d1cf0c33e8d9785cc2f7d
Arlette BELLO <adiath-arlette.bello@campusdulac.com>
2022-05-16 09:51:37 +0200

    finition

--- assets/js/resultat.js
+++ assets/js/resultat.js
@@ -1,9 +1,9 @@
 let result = document.getElementById('result')
 
 if(sessionStorage.getItem('1') == "win"){
-   result.innerHTML = "Bravo😍😍 vous êtes sur la bonne voix💪 !"
+   result.innerHTML = "Bravo😍😍 vous êtes sur la bonne voie💪 !"
     result.style.backgroundColor = "green"
 }else{
-    result.innerHTML = "Dommage vous avez perdu💔! Réeesez"
+    result.innerHTML = "Dommage vous avez perdu💔! Réessayé"
     result.style.backgroundColor = "red"
 }
